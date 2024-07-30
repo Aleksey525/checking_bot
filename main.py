54525abf8f4e4efc8b603100391e8e162f427970
Aleksey525 <alex_tolchin@mail.ru>
2024-07-30 22:07:04 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from environs import Env
+import logging
 import requests
 import telegram
 import time
@@ -7,13 +8,33 @@ import time
 RECONNECTION_DELAY = 30
 
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 def main():
     env = Env()
     env.read_env()
     bot_token = env.str('TG_BOT_TOKEN')
+    bot_logger_token = env.str('TG_BOT_LOGGER_TOKEN')
     bot = telegram.Bot(token=bot_token)
-    devman_token = env.str('DEVMAN_TOKEN')
+    bot_logger = telegram.Bot(token=bot_logger_token)
     chat_id = env.str('TG_CHAT_ID')
+    devman_token = env.str('DEVMAN_TOKEN')
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.DEBUG)
+    telegram_handler = TelegramLogsHandler(bot_logger, chat_id)
+    telegram_handler.setLevel(logging.DEBUG)
+    logger.addHandler(telegram_handler)
+    logger.info('Бот запущен')
     timestamp = ''
     while True:
         try:
